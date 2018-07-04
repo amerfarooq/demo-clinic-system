37a10842aa5b18b40593c6fcdbf5c322deba0e06
Govinda Kumar <govindakumar3210@gmail.com>
2018-07-04 13:36:53 +0500

    pycache

--- clinic/settings.py
+++ clinic/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'supervisor',
+    'home',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- supervisor/models.py
+++ supervisor/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    patient_name = models.CharField(max_length=128)
+    date_of_birth = models.DateField()
+
+class Doctor(models.Model):
+    doc_name = models.CharField(max_length=128)
+    doc_speciality = models.CharField(max_length=128)
+
+class Clinic(models.Model):
+    clinic_name = models.CharField(max_length=128)
+
+class Doc_Clinic(models.Model):
+    doc_name = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    clinic_name = models.ForeignKey(Clinic,on_delete=models.CASCADE)
+
+class Appointment(models.Model):
+    patient_name = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    doc_name = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    clinic_name = models.ForeignKey(Clinic,on_delete=models.CASCADE)
+    appt_date = models.DateField()	
+    appt_time = models.TimeField()	
