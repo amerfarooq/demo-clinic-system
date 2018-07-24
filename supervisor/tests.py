a67e4c3c6c31385ad41b1434bbbf95302ebd3053
Govinda Kumar <govindakumar3210@gmail.com>
2018-07-24 13:43:07 +0500

    unittesting1

--- supervisor/tests.py
+++ supervisor/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from supervisor.models import Doctor
 
 # Create your tests here.
+class DoctorTestcase(TestCase):
+        def setUp(self):
+                self.Obj = Doctor(doctor_name = "John")
+        def test_name(self):
+                self.assertEqual(self.Obj.doctor_name,"John")
