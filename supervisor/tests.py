9797594a496ef10b0dfa98130be92c00291a06ec
Govinda Kumar <govindakumar3210@gmail.com>
2018-07-24 13:43:59 +0500

    unittesting2

--- supervisor/tests.py
+++ supervisor/tests.py
@@ -4,6 +4,7 @@ from supervisor.models import Doctor
 # Create your tests here.
 class DoctorTestcase(TestCase):
         def setUp(self):
+                #to create instances of models that are used in other test methods
                 self.Obj = Doctor(doctor_name = "John")
         def test_name(self):
-                self.assertEqual(self.Obj.doctor_name,"John")
+                self.assertEqual(self.Obj.doctor_name,"not John")
